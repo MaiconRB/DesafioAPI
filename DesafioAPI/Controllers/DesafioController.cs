f25b01e05cc1076406aac744df06b1a47275bf09
MaiconRB <contato.maiconrb@gmail.com>
2023-07-28 21:43:36 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioAPI/Controllers/DesafioController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DesafioController : Controller
+    {
+        // GET
+        [HttpGet]
+        public ActionResult GetNumero()
+        {
+            Random randNum = new Random();
+
+            return Ok(randNum.Next());
+        }
+
+        // POST
+        [HttpPost]
+        public ActionResult PostOperacao(string data, string nome, int primeiroValor, int segundoValor, string operadorMatematico)
+        {
+            var resultado = 0;
+
+            if (operadorMatematico.Equals("+"))
+            {
+                resultado = primeiroValor + segundoValor;
+            }else if(operadorMatematico.Equals("-"))
+                {
+                resultado = primeiroValor - segundoValor;
+            }else if (operadorMatematico.Equals("*"))
+            {
+                resultado = primeiroValor * segundoValor;
+            }
+            else if (operadorMatematico.Equals("/"))
+            {
+                resultado = primeiroValor / segundoValor;
+            }
+
+            return Ok(resultado);
+        }
+
+    }
+}
